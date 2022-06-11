b7f9c2be3c6e2ad6746f3ebddecd5d77300cd9ba
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-12 01:34:24 +0530

    Refactored : Application Properties & Logging Levels

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,19 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		System.out.println("Welcome to Address Book");
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Addressbook App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Addressbook App started");
 	}
 
 }
