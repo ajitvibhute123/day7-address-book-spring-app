bcb362df80dadce5d5284b6b2c5b3aaf8d34af11
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-12 12:47:48 +0530

    Added : Validation To Name Field For REST Calls

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressbookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressbookDTO.java
@@ -2,9 +2,15 @@ package com.bridgelabz.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class AddressbookDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Address-book name is Invalid")
+    @NotEmpty(message = "Address-book name cannot be null")
     public String name;
+    @Pattern(regexp = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$",message = "phone-number is invalid")
     public String phNumber;
 
     public AddressbookDTO(String name, String phNumber) {
